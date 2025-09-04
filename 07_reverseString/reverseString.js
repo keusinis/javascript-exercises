6afe4e34aecff5b16a360ffdc60964f08b0356ae
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-04 14:42:48 +0300

    Solve task 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+// const reverseString = function(str) {
+//     let arr = str.split('');
+//     let reverseArr = [];
+//     for (let i = --str.length; i >= 0; i--) {
+//         reverseArr.push(arr[i]);
+//     }
+//     return reverseArr.join('');
+// };
 
-};
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
