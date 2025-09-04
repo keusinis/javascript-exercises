c86a1c70269d4468e5833032ee1926c5cb4291bc
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-04 14:29:42 +0300

    Solve task 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatNum) {
+    if (repeatNum < 0)
+        return "ERROR";
+    let accumulator = '';
+    for (let i = 0; i < repeatNum; i++)
+        accumulator += str;
+    return accumulator;
 };
+
+// const repeatString = function(str, repeatNum) {
+//     if (repeatNum <   0)
+//         return "ERROR";
+//     if (repeatNum === 0)
+//         return '';
+//     if (repeatNum === 1)
+//         return str;
+//     return str + repeatString(str, --repeatNum);
+// }
 
 // Do not edit below this line
 module.exports = repeatString;
