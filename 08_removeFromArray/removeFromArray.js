cdea35647a802fe12416bf356be9bb3a1b7e865a
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-04 15:24:33 +0300

    Solve task 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter(element => !toRemove.includes(element));
 };
 
 // Do not edit below this line
