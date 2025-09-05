35a9da4707847a401578df63cd40707cdf8248b2
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-05 14:59:32 +0300

    Solve task 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0)
+        return "ERROR";
+    if(x > y)
+        [y, x] = [x, y];
+    let sum = 0;
+    for(let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
