b9e6d01e6e041bd910b30a22e5538bdc48916d7e
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-19 16:45:02 +0300

    Solve task 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, current) => accumulator *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  // let ans = 1;
+  // for(let i = 0; i < num2; i++) {
+  //   ans *= num1;
+  // }
+	// return ans;
+
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // let fact = 1;
+  // for(let i = 2; i <= num; i++) {
+  //   fact *= i;
+  // }
+  // return fact;
+
+  if (num === 0)
+    return 1;
+  if (num === 2)
+    return num;
+	return num * factorial(--num);
 };
 
 // Do not edit below this line
